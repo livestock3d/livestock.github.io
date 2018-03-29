1e0741a0eb7b21022d0f7471506df1a2848ace8d
ocni-dtu <ocni@dtu.dk>
2018-03-29 19:34:23 +0200

    minor fixes

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,14 +2,14 @@ import datetime
 
 AUTHOR = 'Christian Kongsgaard'
 SITENAME = 'Livestock3D'
-SITEURL = ''
+SITEURL = 'https://livestock3d.github.io'
 
 PATH = 'content'
 PAGE_PATHS = ['pages']
 ARTICLE_PATHS = ['posts']
 
 # Top menus
-DISPLAY_CATEGORIES_ON_MENU = False
+DISPLAY_CATEGORIES_ON_MENU = True
 DISPLAY_PAGES_ON_MENU = True
 
 TIMEZONE = 'Europe/Paris'
@@ -22,16 +22,6 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
-
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
@@ -50,7 +40,7 @@ MINIMALXY_CURRENT_YEAR = datetime.date.today().year
 AUTHOR_INTRO = u'Hi everybody! Welcome to Livestock3D!'
 AUTHOR_DESCRIPTION = u"I'm a former Master Student of the Technical University of Denmark (DTU)." \
                      u"\nI'm developed Livestock for my thesis."
-AUTHOR_AVATAR = 'contents/images/profile.jpg'
+AUTHOR_AVATAR = 'https://github.com/livestock3d/livestock3d.github.io/blob/master/images/profil.jpg'
 AUTHOR_WEB = 'http://ocni-dtu.github.io'
 
 JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
